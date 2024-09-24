165d31dece7b2301a154771de8ac88d0541f3598
Maciel Santos <132824821+macielphp@users.noreply.github.com>
2024-09-24 15:52:48 -0300

    Dinamic rendering is completed in CardProject

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './components/Header';
+import { useEffect, useState } from "react";
 import IntroBanner from './components/IntroBanner';
 import SocialsLineY from './components/SocialsLineY'; 
 import Quote from './components/Quote';
@@ -6,9 +7,30 @@ import Title from './components/Title';
 import CardProject from './components/CardProject';
 import './App.css'
 
-
 function App() {
   
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    const gistUrl = 'https://gist.githubusercontent.com/macielphp/d17682882ed9b54e077fedaf2e4ea059/raw/157903d729d2a0ef0c722eb4c739d6451417a60d/gistfile1.txt';
+  
+    fetch(gistUrl)
+      .then(response => {
+        console.log('Resposta bruta:', response);
+        return response.text(); // Usar 'text' para ver o conteúdo como string
+      })
+      .then(text => {
+        console.log('Conteúdo bruto:', text); // Mostra o JSON como texto
+        try {
+          const data = JSON.parse(text); // Tenta fazer o parse do texto
+          setProjects(data.project);
+        } catch (error) {
+          console.error('Erro ao fazer o parse do JSON:', error);
+        }
+      })
+      .catch(error => console.error('Erro ao buscar os dados:', error));
+  }, []);
+
   return (
     <>
       <Header />
@@ -18,12 +40,21 @@ function App() {
         quote='With great power comes great electricity bill.'
         author='Dr. Who'
       />
+      
       <Title titleType='h2' titleText='project' showViewAll='true' />
-      <section className='section'>
-        <CardProject/>
 
-        <CardProject/>
-        <CardProject/>
+      <section className='section'>
+        {projects.map((project, index) => (
+          <CardProject
+            key={index}
+            title={project.title}
+            description={project.description}
+            technologies={project.technologies}
+            githubUrl={project.githubUrl}
+            imageUrl={project.imageUrl}
+            imageAlt={project.imageAlt}
+          />
+        ))}
       </section>
     </>
   )
--- src/components/CardProject/index.jsx
+++ src/components/CardProject/index.jsx
@@ -1,21 +1,35 @@
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from 'react';
 import Picture from '../Picture';
-import './style.css';
-import ProjectoAssociacaoPicture from './assets/picture-default.jpg' 
+import './style.css'; 
 import AncorButton from '../AncorButton';
 import { GoCodeSquare } from "react-icons/go";
+import defaultImage from './assets/default-image.png'
 
-const technologies = ['HTML', 'SCSS', 'JS', 'React JS', 'Vite JS', 'Python', 'Flask', 'Node JS'];
+const CardProject = ({ title, description, technologies, githubUrl, imageUrl, imageAlt }) => {
+    const [validImageUrl, setValidImageUrl] = useState(defaultImage)
 
+    useEffect(() => {
+        const img = new Image();
+        img.src= imageUrl;
 
-const CardProject = () => {
+        img.onload = () => {
+            setValidImageUrl(imageUrl)
+        }
+
+        img.onerror = () => {
+            setValidImageUrl(defaultImage)
+            console.error('Failed to load image:', imageUrl)
+        }
+    })
 
     return(
         <div className='card-roject'>
             <div className='card-project__header'>
                 <Picture 
                     className='card-project__header__image'
-                    imageSrc={ProjectoAssociacaoPicture}
-                    imageAlt={'Card project image '} />
+                    imageSrc={validImageUrl}
+                    imageAlt={imageAlt} />
             </div>
             <div className='card-project__stack'>
                 {technologies.map((tech, index) => (
@@ -23,9 +37,9 @@ const CardProject = () => {
                 ))}
             </div>
             <div className='card-project__body'>
-                <h3>Associação Beneficente</h3>
-                <p>Stock management of a beneficient association</p>
-                <AncorButton href={'https://github.com/macielphp/Projeto_Associacao'} icon={ GoCodeSquare }>GitHub</AncorButton>
+                <h3>{title}</h3>
+                <p>{description}</p>
+                <AncorButton href={githubUrl} icon={ GoCodeSquare }>GitHub</AncorButton>
 
             </div>
         </div>
