d6d6c4e143765ca05f61fcd9ff22749f2df3313f
Maciel Santos <132824821+macielphp@users.noreply.github.com>
2025-02-19 17:34:50 -0300

    Refactor i18n setup: move translation logic to i18n.jsx and remove old i18next.js file

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Works from './pages/works.jsx'
 const root = document.getElementById('root');
 import { DataProvider } from './DataContext.jsx'
+import './translation/i18n'
 
 ReactDOM.createRoot(root).render(
   <DataProvider>
