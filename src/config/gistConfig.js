6484317f87724f1b4e0cb210c63986a926ba5e5a
Maciel Santos <132824821+macielphp@users.noreply.github.com>
2025-04-12 16:31:13 -0300

    Update GIST_URL to point to the latest version of the portfolio

--- src/config/gistConfig.js
+++ src/config/gistConfig.js
@@ -1,3 +1,3 @@
-const GIST_URL = 'https://gist.githubusercontent.com/macielphp/4bebe674fc99f9c62c136acf1778ba5f/raw/15446615e4743e628f1c517d7e84dcf9de40f070/maciel-alves-portfolio(en,%2520pt,%2520es,%2520fr)'
+const GIST_URL = 'https://gist.githubusercontent.com/macielphp/4bebe674fc99f9c62c136acf1778ba5f/raw/30bd865fc39641262c2a082f96e64a0b6d5dd2c4/maciel-alves-portfolio(en,%2520pt,%2520es,%2520fr)'
 
 export default GIST_URL;
